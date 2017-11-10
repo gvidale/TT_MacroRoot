9f3a854465c6d3560b12d65c12519b3cb4dbc480
gvidale <gvidale@VM>
2017-11-10 17:08:44 +0100

    test

--- roads_draw_confront_effFOUNTFLOW_sf.C
+++ roads_draw_confront_effFOUNTFLOW_sf.C
@@ -31,8 +31,8 @@ void roads_draw_confront_effFOUNTFLOW_sf(string file_list_fount, string file_lis
 
 
 	string line;
-	ifstream  myfilefount;
-	ifstream myfileflow;
+	ifstream  myfilefount(file_list_fount);
+	ifstream myfileflow(file_list_flower);
 
 	ifstream * myfile[0];
 	myfile[0] = & myfilefount;
