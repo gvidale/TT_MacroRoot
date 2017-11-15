002883ea00f9238ab11db655cd3d2628a888fb72
gvidale <gvidale@VM>
2017-11-15 20:28:42 +0100

    sf07

--- read_patternBank_coverage.C
+++ read_patternBank_coverage.C
@@ -77,8 +77,8 @@ void read_patternBank_coverage(){
 //
 				"TT33_muPU200_flowonly_sf1_sf1_pt08_100_2610_bank",
 				"TT33_muPU200_fount_sf1_sf1_100_2610_bank",
-				"TT33_muPU200_fount_sf1_sf07_100_2610_bank",
-				"TT33_muPU200_flowonly_sf1_sf07_pt08_100_2610_bank"};
+				"TT33_muPU200_fount_sf07_sf1_100_2610_bank",
+				"TT33_muPU200_flowonly_sf07_sf07_pt08_100_2610_bank"};
 
 //	fName = "../patternBank/"+name+".root"; //name of file root to read, with path
 //	key = "_"+name;     //just a key for histos
